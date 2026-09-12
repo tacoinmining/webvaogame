0b12416863db168dee65cce768d248e27aefb101
anhtuan130809 <nguyenanhtuan1389@gmail.com>
2026-09-12 21:04:11 +0700

    l

---
+++ components/ui/demo-loader.tsx
@@ -0,0 +1,5 @@
+import { Component } from "@/components/ui/loader-2";
+
+export default function DemoOne() {
+  return <Component />;
+}
---
+++ components/ui/loader-2.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+export const Component = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="loader">
+        <svg viewBox="0 0 80 80">
+          <circle r="32" cy="40" cx="40" id="test"></circle>
+        </svg>
+      </div>
+
+      <div className="loader triangle">
+        <svg viewBox="0 0 86 80">
+          <polygon points="43 8 79 72 7 72"></polygon>
+        </svg>
+      </div>
+
+      <div className="loader">
+        <svg viewBox="0 0 80 80">
+          <rect height="64" width="64" y="8" x="8"></rect>
+        </svg>
+      </div>
+    </>
+  );
+};
